550db476a57a9f76ad98a930fdda091fdf57f6ab
riteshar7 <ritesh.ranjan@iitg.ac.in>
2024-08-20 19:35:45 +0530

    Did Minor Fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Board from "./components/Board";
 import Toolbar from "./components/Toolbar";
 import Toolbox from "./components/Toolbox";
-import BoardProvider from "./store/BoardProvider";
-import ToolboxProvider from "./store/ToolboxProvider";
+import BoardProvider from "./store/boardProvider";
+import ToolboxProvider from "./store/ToolBoxProvider";
 
 function App() {
   return (
